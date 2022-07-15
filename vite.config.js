32cab4b35480f959cdd1f6b78d4aac5d5754013f
Nick McCurdy <nick@nickmccurdy.com>
2022-07-15 08:48:23 -0400

    Use prettier-config-nick

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,25 @@
-import { defineConfig } from 'vitest/config'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vitest/config"
+import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    open: true
+    open: true,
   },
   build: {
     sourcemap: true,
     commonjsOptions: {
-      include: []
-    }
+      include: [],
+    },
   },
   optimizeDeps: {
-    disabled: false
+    disabled: false,
   },
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: '@testing-library/jest-dom',
-    mockReset: true
-  }
+    environment: "jsdom",
+    setupFiles: "@testing-library/jest-dom",
+    mockReset: true,
+  },
 })
