d31d9cd740a920ea19f29a52e3b8b984644baa4f
Nick McCurdy <nick@nickmccurdy.com>
2022-07-15 15:12:53 -0400

    Add ESLint to build (fix #5)

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
 import { defineConfig } from "vitest/config"
+import checker from "vite-plugin-checker"
 import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    checker({
+      eslint: {
+        lintCommand: "eslint .",
+      },
+    }),
+  ],
   server: {
     open: true,
   },
