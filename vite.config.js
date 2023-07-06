2185ca1c290bfac5aeb476d1f7f0fd8e3d4ef341
Nick McCurdy <nick@nickmccurdy.com>
2023-07-06 07:57:53 -0400

    Switch to `prettier-config-a11y`

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,20 @@
-import { defineConfig } from "vitest/config"
-import react from "@vitejs/plugin-react"
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    open: true,
-  },
-  build: {
-    outDir: "build",
-    sourcemap: true,
-  },
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "@testing-library/jest-dom",
-    mockReset: true,
-  },
-})
+	plugins: [react()],
+	server: {
+		open: true,
+	},
+	build: {
+		outDir: "build",
+		sourcemap: true,
+	},
+	test: {
+		globals: true,
+		environment: "jsdom",
+		setupFiles: "@testing-library/jest-dom",
+		mockReset: true,
+	},
+});
